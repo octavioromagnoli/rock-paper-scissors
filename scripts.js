67b8f9054adc7d45bef9eed8d146980da587de3f
octavioromagnoli <octaromag@gmail.com>
2024-07-23 03:05:38 -0300

    Añado interaccion UI y contador de rondas
    Ahora el juego se juega con los botones y al
    alguno ganar 5 rondas muestra un mensaje y se reinicia.

--- scripts.js
+++ scripts.js
@@ -1,34 +1,48 @@
-function getComputerChoice(){
+let compu = 0;
+let pipol = 0;
+let empates = 0;
+
+function getComputerChoice() {
   let choice = Math.random();
-  if (choice < 0.33){
+  if (choice < 0.33) {
     return "Rock";
-  } else if (choice > 0.66){
+  } else if (choice > 0.66) {
     return "Scissors";
-  } else {return "Paper";}
+  } else { return "Paper"; }
 }
 
-function playGame(input){
+function playGame(input) {
   let computerMove = getComputerChoice();
   let result = "";
-  switch (computerMove){
+  switch (computerMove) {
     case "Rock":
-      if (input == "Rock"){ result = "It's a Tie";}
-      if (input == "Scissors"){ result = "Computer Wins :(";}
-      if (input == "Paper"){ result = "You win!!";}
+      if (input == "Rock") { result = "It's a Tie"; empates++;}
+      if (input == "Scissors") { result = "Computer Wins :("; compu++;}
+      if (input == "Paper") { result = "You win!!"; pipol++;}
       break;
     case "Paper":
-      if (input == "Paper"){ result = "It's a Tie";}
-      if (input == "Rock"){ result = "Computer Wins :(";}
-      if (input == "Scissors"){ result = "You win!!";}
+      if (input == "Paper") { result = "It's a Tie";empates++;}
+      if (input == "Rock") { result = "Computer Wins :("; compu++;}
+      if (input == "Scissors") { result = "You win!!"; pipol++;}
       break;
     case "Scissors":
-      if (input == "Scissors"){ result = "It's a Tie";}
-      if (input == "Paper"){ result = "Computer Wins :(";}
-      if (input == "Rock"){ result = "You win!!";}
+      if (input == "Scissors") { result = "It's a Tie"; empates++;}
+      if (input == "Paper") { result = "Computer Wins :("; compu++;}
+      if (input == "Rock") { result = "You win!!"; pipol++;}
       break;
   }
-  return `You:${input}, Computer:${computerMove}. ${result}`;
+  console.log(`You:${input}, Computer:${computerMove}. ${result}
+    puntajes:
+        Vos: ${pipol}
+        Compu: ${compu}
+        Empates: ${empates}`)
+
+  if(pipol === 5){
+    pipol = compu = empates = 0;
+    console.log("GANASTE LA PARTIDA (5 Rondas)");
+  }
+  else if(compu === 5){
+    pipol = compu = empates = 0;
+    console.log("Gano la compu :((( (5 Rondas)")
+  }
 }
-
-
-console.log(playGame("Rock"));
