d2d89862af6a42ecb923c0e30a19c3d77de8dae6
octavioromagnoli <octaromag@gmail.com>
2024-07-24 18:57:00 -0300

    Show game status on the DOM
    Add DOM manipulation to the results div for it to update
    within each round, so that it shows the game status.

--- scripts.js
+++ scripts.js
@@ -16,19 +16,19 @@ function playGame(input) {
   let result = "";
   switch (computerMove) {
     case "Rock":
-      if (input == "Rock") { result = "It's a Tie"; empates++;}
-      if (input == "Scissors") { result = "Computer Wins :("; compu++;}
-      if (input == "Paper") { result = "You win!!"; pipol++;}
+      if (input == "Rock") { result = "It's a Tie"; empates++; }
+      if (input == "Scissors") { result = "Computer Wins :("; compu++; }
+      if (input == "Paper") { result = "You win!!"; pipol++; }
       break;
     case "Paper":
-      if (input == "Paper") { result = "It's a Tie";empates++;}
-      if (input == "Rock") { result = "Computer Wins :("; compu++;}
-      if (input == "Scissors") { result = "You win!!"; pipol++;}
+      if (input == "Paper") { result = "It's a Tie"; empates++; }
+      if (input == "Rock") { result = "Computer Wins :("; compu++; }
+      if (input == "Scissors") { result = "You win!!"; pipol++; }
       break;
     case "Scissors":
-      if (input == "Scissors") { result = "It's a Tie"; empates++;}
-      if (input == "Paper") { result = "Computer Wins :("; compu++;}
-      if (input == "Rock") { result = "You win!!"; pipol++;}
+      if (input == "Scissors") { result = "It's a Tie"; empates++; }
+      if (input == "Paper") { result = "Computer Wins :("; compu++; }
+      if (input == "Rock") { result = "You win!!"; pipol++; }
       break;
   }
   console.log(`You:${input}, Computer:${computerMove}. ${result}
@@ -36,6 +36,47 @@ function playGame(input) {
         Vos: ${pipol}
         Compu: ${compu}
         Empates: ${empates}`)
+  displayResults(input,computerMove,result,pipol,compu,empates);
+}
+
+function displayResults(playerMove, compuMove, result, playerScore, compuScore, ties) {
+  const resultScreen = document.querySelector(".resultScreen");
+  resultScreen.innerHTML = "";
+
+  const movesDisplay = document.createElement("p");
+  movesDisplay.setAttribute("class", "scores playerScore");
+  movesDisplay.textContent = `Jugaste: ${playerMove} || Compu jugó: ${compuMove}. ==> ${result}`;
+
+  const playerSDisplay = document.createElement("span");
+  playerSDisplay.setAttribute("class", "scores playerScore");
+  playerSDisplay.textContent = `You:
+  ${playerScore} `;
+
+  const computerSDisplay = document.createElement("span");
+  computerSDisplay.setAttribute("class", "scores playerScore");
+  computerSDisplay.textContent = `Computer:
+  ${compuScore} `;
+  
+  const tieSDisplay = document.createElement("span");
+  tieSDisplay.setAttribute("class", "scores tieScore");
+  tieSDisplay.textContent = `Empates:
+  ${ties} `;
+
+  
+  resultScreen.appendChild(movesDisplay);
+  resultScreen.appendChild(playerSDisplay);
+  resultScreen.appendChild(computerSDisplay);
+  resultScreen.appendChild(tieSDisplay);
+
+  if (playerScore === 5){
+    let rText = document.createTextNode("GANASTE")
+    resultScreen.appendChild(rText);
+  }
+  else if (compuScore === 5){
+    let rText = document.createTextNode("PERDISTE")
+    resultScreen.appendChild(rText);
+  }
+
 }
 
 const playButtons = document.querySelector(".playButtons");
